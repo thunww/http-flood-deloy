18f817303f64044484564bec95bebe7d9c4b1d35
Tran Gia Than <trangiathandz2004@gmail.com>
2025-12-18 11:46:12 +0700

    add

---
+++ backend/src/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const authRoute = require("./routes/auth.route");
+const productRoute = require("./routes/product.route");
+const promBundle = require("express-prom-bundle");
+
+const app = express(); // ✅ PHẢI ĐỂ TRÊN
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  metricsPath: "/metrics",
+  promClient: {
+    collectDefaultMetrics: {},
+  },
+});
+
+app.use(
+  cors({
+    origin: [
+      "https://noithatstore.site",
+      /\.pages\.dev$/, // wildcard ĐÚNG
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const INSTANCE_ID = process.env.INSTANCE_ID || "unknown-backend";
+
+app.use((req, res, next) => {
+  console.log(`[${INSTANCE_ID}] ${req.method} ${req.url}`);
+  next();
+});
+
+app.use(metricsMiddleware);
+
+app.use("/api", authRoute);
+app.use("/api/products", productRoute);
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "OK",
+    instance: INSTANCE_ID,
+  });
+});
+
+app.listen(8080, () => console.log(`[${INSTANCE_ID}] BE running on 8080`));
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+// frontend/src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
